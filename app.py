d9840666cd693bc2a9d7974efda4780a6ad9e236
yash-shriv <115723600+yash-shriv@users.noreply.github.com>
2024-02-15 02:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,55 +1,43 @@
-# We can use Flask for website development, But here I use Streamlit library to make it easy.
-# Optional: Also try to build website with Flask or Django and add the ML model to it.
-
 import streamlit as st
 import pickle
 import pandas as pd
 import requests
 
-# This function takes a movie id and hits the api. we need to import request lib to hit(request) api
-
 
 def fetch_poster(movie_id):
     response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US'.format(movie_id))
-    # convert response to json
+    
     data = response.json()
-
-    return "https://image.tmdb.org/t/p/w780/asmCFd6BnqPkwIj0jHzcJXgNJvi.jpg" + data['poster_path']
+    data = response.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w780/" + poster_path
+    return full_path
 
 
 def recommend(movie):
     movie_index = movies[movies['title'] == movie].index[0]
-    distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)), reverse=True, key=lambda x: x[1])[1:6]
-
-    recommended_movies_names = []
-    recommended_movies_posters = []
-    for i in movies_list:
+    movies_list = sorted(list(enumerate(similarity[movie_index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in movies_list[1:6]:
         movie_id = movies.iloc[i[0]].movie_id
-        # fetch poster from API, time 01:50:00
-        recommended_movies_names.append(movies.iloc[i[0]].title)
-        recommended_movies_posters.append(fetch_poster(movie_id))
-    return recommended_movies_names, recommended_movies_posters
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
 
-# I have convert dataframe to dictionary
+    return recommended_movie_names, recommended_movie_posters
 
 
 movies_dict = pickle.load(open('movie_dict.pkl', 'rb'))
 movies = pd.DataFrame(movies_dict)
-
 similarity = pickle.load(open('similarity.pkl', 'rb'))
-
 st.title('Movie Recommender System')
 
 selected_movie_name = st.selectbox(
-    'Type or select a movie from the dropdown',
+    "Type or select a movie from the dropdown",
     movies['title'].values)
 
-# Add button (Take reference from streamlit documentation for creating/doing anything on website.)
-
-if st.button('Recommend'):
+if st.button('Show Recommendation'):
     names, posters = recommend(selected_movie_name)
-
     col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(names[0])
@@ -57,9 +45,6 @@ if st.button('Recommend'):
     with col2:
         st.text(names[1])
         st.image(posters[1])
-    with col3:
-        st.text(names[2])
-        st.image(posters[2])
     with col3:
         st.text(names[2])
         st.image(posters[2])
